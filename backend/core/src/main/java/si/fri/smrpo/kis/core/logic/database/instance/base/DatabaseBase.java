b3f560491c49fa5afcd6ee1bdb6289882cfe623e
Jan Gasperlin <jg1724@student.uni-lj.si>
2018-03-22 01:50:35 +0100

    Re added Jinq.

--- backend/core/src/main/java/si/fri/smrpo/kis/core/logic/database/instance/base/DatabaseBase.java
+++ backend/core/src/main/java/si/fri/smrpo/kis/core/logic/database/instance/base/DatabaseBase.java
@@ -1,11 +1,18 @@
 package si.fri.smrpo.kis.core.logic.database.instance.base;
 
+import org.jinq.jpa.JPAJinqStream;
+import org.jinq.jpa.JinqJPAStreamProvider;
+import si.fri.smrpo.kis.core.jpa.BaseEntity;
+
 import javax.persistence.EntityManager;
+import java.io.Serializable;
 import java.util.logging.Logger;
 
 public abstract class DatabaseBase implements DatabaseBaseImpl {
 
     protected EntityManager entityManager;
+
+    private JinqJPAStreamProvider source;
 
     public DatabaseBase() {
     }
@@ -20,6 +27,13 @@ public abstract class DatabaseBase implements DatabaseBaseImpl {
 
     public EntityManager getEntityManager() {
         return entityManager;
+    }
+
+    public <T extends BaseEntity> JPAJinqStream<T> stream(Class<T> c){
+        if(source == null){
+            source = new JinqJPAStreamProvider(entityManager.getMetamodel());
+        }
+        return source.streamAll(entityManager, c);
     }
 
 }
--- backend/core/src/main/java/si/fri/smrpo/kis/core/logic/database/instance/base/DatabaseBaseImpl.java
+++ backend/core/src/main/java/si/fri/smrpo/kis/core/logic/database/instance/base/DatabaseBaseImpl.java
@@ -1,9 +1,13 @@
 package si.fri.smrpo.kis.core.logic.database.instance.base;
 
+import org.jinq.jpa.JPAJinqStream;
+import si.fri.smrpo.kis.core.jpa.BaseEntity;
+
 import javax.persistence.EntityManager;
 
 public interface DatabaseBaseImpl {
 
     EntityManager getEntityManager();
+    <T extends BaseEntity> JPAJinqStream<T> stream(Class<T> c);
 
 }
