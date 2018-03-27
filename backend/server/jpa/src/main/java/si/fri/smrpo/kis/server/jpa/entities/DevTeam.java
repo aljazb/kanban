96ec31be4cf8f78cc9ce7b39d157f7980c0f4c5b
um7316 <um7316@student.uni-lj.si>
2018-03-27 11:22:58 +0200

    Removed remaining JsonIdentityInfo

--- backend/server/jpa/src/main/java/si/fri/smrpo/kis/server/jpa/entities/DevTeam.java
+++ backend/server/jpa/src/main/java/si/fri/smrpo/kis/server/jpa/entities/DevTeam.java
@@ -29,7 +29,6 @@ import java.util.Set;
                 query = "SELECT ua FROM DevTeam dt JOIN dt.joinedUsers uaMTMdt JOIN uaMTMdt.userAccount ua WHERE dt.id = :id AND dt = uaMTMdt.devTeam AND uaMTMdt.userAccount = ua AND " +
                         "(uaMTMdt.memberType = si.fri.smrpo.kis.server.jpa.enums.MemberType.DEVELOPER OR uaMTMdt.memberType = si.fri.smrpo.kis.server.jpa.enums.MemberType.DEVELOPER_AND_KANBAN_MASTER OR uaMTMdt.memberType = si.fri.smrpo.kis.server.jpa.enums.MemberType.DEVELOPER_AND_PRODUCT_OWNER)"),
 })
-@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class)
 public class DevTeam extends UUIDEntity<DevTeam> {
 
     @Column(name = "name", nullable = false)
