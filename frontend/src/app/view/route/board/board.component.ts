0913d1959c1bf9bf6d2650b3cd5163d88e6492ad
um7316 <um7316@student.uni-lj.si>
2018-04-17 09:42:04 +0200

    Fixed no dev teams, projects or boards

--- frontend/src/app/view/route/board/board.component.ts
+++ frontend/src/app/view/route/board/board.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {Board} from '../../../api/models/Board';
+import {LoginService} from '../../../api/login.service';
 
 @Component({
   selector: 'app-board',
@@ -9,10 +10,12 @@ import {Board} from '../../../api/models/Board';
 })
 export class BoardComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  isKanbanMaster: boolean;
+
+  constructor(private router: Router, private loginService: LoginService) { }
 
   ngOnInit() {
-
+    this.loginService.getUser().subscribe(u => this.isKanbanMaster = u.inRoleKanbanMaster);
   }
 
   goToBoard(board: Board): void {
