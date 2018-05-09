9fe5e42a7abdc50f22aacd39d0395e77d1489d7e
Aljaž <aljaz.blazej@gmail.com>
2018-05-09 09:42:07 +0200

    Workload number limit

--- frontend/src/app/view/components/forms/card-form/card-form.component.ts
+++ frontend/src/app/view/components/forms/card-form/card-form.component.ts
@@ -47,7 +47,7 @@ export class CardFormComponent extends FormImpl {
   initFormControls(): void {
     this.fcName = new FormControl('', Validators.required);
     this.fcDescription = new FormControl('', Validators.required);
-    this.fcWorkload = new FormControl('');
+    this.fcWorkload = new FormControl('', Validators.pattern('([0-9]+)'));
     this.fcColor = new FormControl(null, Validators.required);
   }
 
